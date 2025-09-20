1ed44f5f97eaa88be39d887795ce171c92965b9d
Chris Bartie <st10033223@vcconnect.edu.za>
2025-09-20 10:39:52 +0200

    study session post

--- uniBuddyAPI/uniBuddyAPI/Controllers/StudySessionsController.cs
+++ uniBuddyAPI/uniBuddyAPI/Controllers/StudySessionsController.cs
@@ -15,6 +15,33 @@ namespace uniBuddyAPI.Controllers
             _db = db;
         }
 
+        [HttpPost("{userId}")]
+        public async Task<IActionResult> AddSession(string userId, [FromBody] StudySession body)
+        {
+            var session = new StudySession
+            {
+                UserId = userId,
+                StartTime = body.StartTime == default ? DateTime.UtcNow : body.StartTime,
+                EndTime = body.EndTime,
+                Duration = body.Duration,
+            };
 
+            var response = await _db.Client.PostAsJsonAsync($"/studySession/{userId}.json", session);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var resultContent = await response.Content.ReadAsStringAsync();
+                return Ok(new
+                {
+                    message = "Study session saved successfully",
+                    data = resultContent
+                });
+            }
+            else
+            {
+                return BadRequest(new { message = "Your study session did not save..."});
+            }
+        }
     }
 }
+
