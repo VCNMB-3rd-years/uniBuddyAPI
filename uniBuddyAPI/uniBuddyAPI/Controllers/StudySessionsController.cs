0b7e89b482f81283de8b3da4dad306e04080dfe1
Chris Bartie <st10033223@vcconnect.edu.za>
2025-09-20 15:47:06 +0200

    study timer endpoints (in progress)

--- uniBuddyAPI/uniBuddyAPI/Controllers/StudySessionsController.cs
+++ uniBuddyAPI/uniBuddyAPI/Controllers/StudySessionsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using uniBuddyAPI.Models;
 using uniBuddyAPI.Services;
 
@@ -21,9 +22,9 @@ namespace uniBuddyAPI.Controllers
             var session = new StudySession
             {
                 UserId = userId,
-                StartTime = body.StartTime == default ? DateTime.UtcNow : body.StartTime,
-                EndTime = body.EndTime,
-                Duration = body.Duration,
+                StartTime = body.StartTime == default ? DateTime.UtcNow : body.StartTime, //adds a start time of right now
+                EndTime = body.EndTime, //need to still figure out how to automatically add an end time
+                Duration = body.Duration, //end time minus start time (figure out still)
             };
 
             var response = await _db.Client.PostAsJsonAsync($"/studySession/{userId}.json", session);
@@ -39,8 +40,53 @@ namespace uniBuddyAPI.Controllers
             }
             else
             {
-                return BadRequest(new { message = "Your study session did not save..."});
+                //error message if something goes wrong, app wont crash
+                return BadRequest(new { message = "Your study session did not save..." });
             }
+        }
+
+        [HttpGet("{userId}")] //getting the logged in users study sessions
+        public async Task<IActionResult> GetSessions(string userId)
+        {
+            var response = await _db.Client.GetAsync($"/studySession/{userId}.json");
+            if (!response.IsSuccessStatusCode) return BadRequest(new { message = "Could not load your study sessions" });
+
+            var json = await response.Content.ReadAsStringAsync();
+            //returning a message to the user if they have no recorded study sessions
+            if (string.IsNullOrWhiteSpace(json) || json == "null")
+                return NotFound(new { message = "You have no recorded study sessions" });
+
+            Dictionary<string, StudySession>? map;
+            try
+            {
+                //Code Attribution
+                //The PropertyNameCaseInsensitive option has been created with the help of StackOverflow
+                //https://stackoverflow.com/questions/45782127/json-net-case-insensitive-deserialization-not-working
+                //Ziaullah Khan
+                //https://stackoverflow.com/users/3312570/ziaullah-khan
+                map = JsonSerializer.Deserialize<Dictionary<string, StudySession>>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+            catch
+            {
+                return Ok(new List<StudySession>());
+            }
+
+            var list = new List<StudySession>();
+            if (map != null)
+            {
+                foreach (var (key, value) in map)
+                {
+                    //firebase key is the session id
+                    value.SessionId = string.IsNullOrWhiteSpace(value.SessionId) ? key : value.SessionId; //session id is the firebase key
+                    list.Add(value);
+                }
+            }
+
+            //returning the most recent start time first in the list of study sessions
+            return Ok(list.OrderByDescending(s => s.StartTime).ToList());
         }
     }
 }
