82ec74432b71dac00a6561214ce5981873a36d10
Chris Bartie <st10033223@vcconnect.edu.za>
2025-10-02 13:35:53 +0200

    api takes in users email now too

--- uniBuddyAPI/uniBuddyAPI/Models/LoginResponse.cs
+++ uniBuddyAPI/uniBuddyAPI/Models/LoginResponse.cs
@@ -6,5 +6,6 @@
         public string UserId { get; set; }
         public string name { get; set; }
         public string surname { get; set; }
+        public string email { get; set; }
     }
 }
